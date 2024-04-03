308f47ee7084ba9305fb8718b53ea3913a8d05a0
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2024-04-03 13:33:59 +0200

    Activate Hot Reloading

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import ViteRails from 'vite-plugin-rails'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
+    ViteRails(
+      {
+        fullReload: {
+          additionalPaths: ["config/routes.rb", "app/views/**/*"],
+          delay: 200
+        },
+      }
+    ),
   ],
 })
