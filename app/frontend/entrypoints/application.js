665d1d8232aa92af6dae806afe31a76fb0f9064f
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2024-04-03 14:11:56 +0200

    Add Flowbite

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -26,3 +26,4 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 import '~/stylesheets/application.css'
+import '~/application.js'
